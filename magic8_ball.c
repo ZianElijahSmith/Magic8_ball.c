1bae2037400a95e81faacd9611ed9dee3bb92b24
Zian Elijah Smith <121137084+ZianElijahSmith@users.noreply.github.com>
2023-04-03 14:34:16 -0400

    Update magic8_ball.c

--- magic8_ball.c
+++ magic8_ball.c
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 
-/***************************************************************************************/
-/* Program name:  Magic_8ball.c                                                        */         
-/* Program purpose: To simulate a Magic 8ball for fun                                  */
-/* Date created:  Sunday, March 26th, 2023 (3/26/2023)                                 */
-/* Program Author: Zian Elijah Smith                                                   */
-/*                                                                                     */
-/* Compile Options:                                                                    */
-/*                                                                                     */
-/* clang -std=gnu17 -W -Wall -O -fsanitize=address,undefined magic-8ball.c -o          */
-/*                                                                                     */
-/* gcc -std=gnu17 -W -Wall -O -fsanitize=address,bounds-strict magic-8ball.c -o test   */
-/*                                                                                     */
-/* Made this while learning C                                                          */
-/***************************************************************************************/
+/***********************************************************************************************************/
+/* Program name:  Magic_8ball.c                                                                            */         
+/* Program purpose: To simulate a Magic 8ball for fun                                                      */
+/* Date created:  Sunday, March 26th, 2023 (3/26/2023)                                                     */
+/* Program Author: Zian Elijah Smith                                                                       */
+/*                                                                                                         */
+/* Compile Options:                                                                                        */
+/*                                                                                                         */
+/* clang -std=c17 -W -O -Wextra -pedantic -fsanitize=address,undefined magic-8ball.c -o test               */
+/*                                                                                                         */
+/* gcc -std=c17 -Wall -Wextra -pedantic -fsanitize=address,bounds-strict,undefined magic-8ball.c -o test   */
+/*                                                                                                         */
+/* Made this while learning C                                                                              */
+/***********************************************************************************************************/
 
 int main(void)
 {
